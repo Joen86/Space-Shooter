1a3d79cae7130bbdac12c9b7c65ec0fcff11d4fa
Joe Sang Nguyen <sang.nguyen@rutgers.edu>
2015-06-11 00:04:15 -0400

    Added Restart and Game Over
    getting error and not able to find why, restart and game over text not
    seen in game controller inspector

--- Space Shooter/Assets/Scripts/DestroyByContact.cs
+++ Space Shooter/Assets/Scripts/DestroyByContact.cs
@@ -2,12 +2,12 @@
 using System.Collections;
 
 public class DestroyByContact : MonoBehaviour {
-
+	
 	public GameObject explosion;
 	public GameObject playerExplosion;
 	public int scoreValue;
 	private GameController gameController;
-
+	
 	void Start ()
 	{
 		GameObject gameControllerObject = GameObject.FindWithTag ("GameController");
@@ -20,7 +20,7 @@ public class DestroyByContact : MonoBehaviour {
 			Debug.Log ("Cannot find 'GameController' script");
 		}
 	}
-
+	
 	void OnTriggerEnter(Collider other) 
 	{
 		if (other.tag == "Boundary")
@@ -29,11 +29,13 @@ public class DestroyByContact : MonoBehaviour {
 		}
 		Instantiate(explosion, transform.position, transform.rotation);
 		if (other.tag == "Player") {	
-						Instantiate (playerExplosion, other.transform.position, other.transform.rotation);
-				}
+			Instantiate (playerExplosion, other.transform.position, other.transform.rotation);
+			gameController.GameOver();
+		}
 		gameController.AddScore (scoreValue);
 		Destroy(other.gameObject);
 		Destroy(gameObject);
 	}
-
+	
+	
 }
